392c0d1982b282a50919e2ee507ad94f9c77dd43
Lukas Schmidt <lukas.schmi@gmail.com>
2015-03-31 14:43:18 +0200

    test

--- TagSearcher.py
+++ TagSearcher.py
@@ -1,10 +1,11 @@
 __author__ = 'Lukas und Max K.'
+from sets import Set
 
 class TagSearcher:
 	tagConfigSet = Set()
 	def __init__(self, tagConfigList):
 		for configTag in tagConfigList:
-			tagConfigSet.add(configTag)
+			self.tagConfigSet.add(configTag)
 	
-	def containsDicomTagInConfig(dicomTag):	
-		return dicomTag in tagConfigSet
+	def containsDicomTagInConfig(self, dicomTag):	
+		return dicomTag in self.tagConfigSet
---
+++ TagSearcherTest.py
@@ -0,0 +1,20 @@
+import unittest
+from TagSearcher import TagSearcher
+
+configTagList = ["98675412", "89A042B1", "11457863"]
+
+class TestStringMethods(unittest.TestCase):
+	tagSearcher = TagSearcher(configTagList)
+	def test_valid_instace(self):
+	  self.assertIsInstance(self.tagSearcher, TagSearcher)
+	
+	def test_contains_tag(self):
+		tag = configTagList[1]
+		self.assertTrue(self.tagSearcher.containsDicomTagInConfig(tag))
+		self.assertTrue(self.tagSearcher.containsDicomTagInConfig(configTagList[0]))
+	
+	def test_should_not_contain_tag(self):
+		self.assertFalse(self.tagSearcher.containsDicomTagInConfig("0123"))
+
+if __name__ == '__main__':
+	unittest.main()
