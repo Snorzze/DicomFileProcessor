c49ea7de8a0cbe2725ae7cde05f7aede3a661d33
mkursche <mkursche@stud.hs-heilbronn.de>
2015-04-06 18:32:50 +0200

    TagSearcherTest setUp Method

--- test/TagSearcherTest.py
+++ test/TagSearcherTest.py
@@ -1,20 +1,27 @@
 import unittest
+
 from TagSearcher import TagSearcher
+
 
 configTagList = ["98675412", "89A042B1", "11457863"]
 
 class TestStringMethods(unittest.TestCase):
-	tagSearcher = TagSearcher(configTagList)
-	def test_valid_instace(self):
-	  self.assertIsInstance(self.tagSearcher, TagSearcher)
-	
-	def test_contains_tag(self):
-		tag = configTagList[1]
-		self.assertTrue(self.tagSearcher.containsDicomTagInConfig(tag))
-		self.assertTrue(self.tagSearcher.containsDicomTagInConfig(configTagList[0]))
-	
-	def test_should_not_contain_tag(self):
-		self.assertFalse(self.tagSearcher.containsDicomTagInConfig("0123"))
+    def setUp(self):
+        self.tagSearcher = TagSearcher(configTagList)
+
+    def test_valid_instace(self):
+        self.assertIsInstance(self.tagSearcher, TagSearcher)
+        print("test_valid_instance")
+
+    def test_contains_tag(self):
+        tag = configTagList[1]
+        self.assertTrue(self.tagSearcher.containsDicomTagInConfig(tag))
+        self.assertTrue(self.tagSearcher.containsDicomTagInConfig(configTagList[0]))
+        print("test_contains_tag")
+
+    def test_should_not_contain_tag(self):
+        self.assertFalse(self.tagSearcher.containsDicomTagInConfig("0123"))
+        print("test_should_not")
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
