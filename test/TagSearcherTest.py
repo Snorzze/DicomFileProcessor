8f3bfada7b538033953442a43892ba1d5dd619d6
mkursche <mkursche@stud.hs-heilbronn.de>
2015-04-06 23:34:37 +0200

    refactored print statements

--- test/TagSearcherTest.py
+++ test/TagSearcherTest.py
@@ -11,17 +11,14 @@ class TestStringMethods(unittest.TestCase):
 
     def test_valid_instace(self):
         self.assertIsInstance(self.tagSearcher, TagSearcher)
-        print("test_valid_instance")
 
     def test_contains_tag(self):
         tag = configTagList[1]
         self.assertTrue(self.tagSearcher.containsDicomTagInConfig(tag))
         self.assertTrue(self.tagSearcher.containsDicomTagInConfig(configTagList[0]))
-        print("test_contains_tag")
 
     def test_should_not_contain_tag(self):
         self.assertFalse(self.tagSearcher.containsDicomTagInConfig("0123"))
-        print("test_should_not")
 
 if __name__ == '__main__':
     unittest.main()
