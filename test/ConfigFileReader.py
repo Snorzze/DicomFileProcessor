a4b5f37d774c39f7c6cc9eb4c58eb4fb9b1c8f96
thagel <thomashagel@gmx.net>
2015-04-20 13:32:32 +0200

    Test hinzugefügt

---
+++ test/ConfigFileReader.py
@@ -0,0 +1,15 @@
+class ConfigFileReader:
+    def readConfig(self, path):
+        text_file = open(path, "r")
+        content = text_file.read().strip().split(",")
+        return list(map(self.turn, content))
+
+    def turn(self, tag):
+        groupId = self.swap(tag[:-4])
+        elementId = self.swap(tag[-4:])
+        return groupId + elementId
+
+    def swap(self, tagHalf):
+        part1 = tagHalf[:-2]
+        part2 = tagHalf[-2:]
+        return part2 + part1
