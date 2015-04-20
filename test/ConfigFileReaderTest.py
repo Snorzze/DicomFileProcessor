032ddf59721a471097c0d522d16aa7f88d9197af
thagel <thomashagel@gmx.net>
2015-04-20 13:28:56 +0200

    Test hinzugefügt

---
+++ test/ConfigFileReaderTest.py
@@ -0,0 +1,17 @@
+import unittest
+
+from ConfigFileReader import ConfigFileReader
+
+path = "ConfigSamples/configSample1.txt"
+
+class TestConfigFileReader(unittest.TestCase):
+    def setUp(self):
+        self.configFileReader = ConfigFileReader()
+
+    def test_readConfig(self):
+        result = self.readConfig(path)
+        validResult = ['18005011', '18005111', '18005211']
+        self.assertEquals(result, validResult)
+
+if __name__ == "__main__":
+    unittest.main()
