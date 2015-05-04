a9d667cd15d54ec589db8b0bd35e7cd861458d96
Lukas Schmidt <lukas.schmi@gmail.com>
2015-05-04 14:22:48 +0200

    working tag exporter

--- TagExporter.py
+++ TagExporter.py
@@ -4,7 +4,35 @@ class TagExporter:
     exportData = {}
 
     def saveTag(self, tag, value, file):
-        if tag not in self.exportData:
-            self.exportData[tag] = []
-        data = {"value": value, "file": file}
-        self.exportData[tag].append(data)
+        if file not in self.exportData:
+            self.exportData[file] = {}
+        data = {"tag": tag, "value": value}
+        self.exportData[file][tag] = data
+
+    def writeToFile(self, f, tags):
+        fieldnames = ['first_name', 'last_name']
+        with open('names.csv', 'w') as csvfile:
+            fieldnames = ['file'] + tags
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+            writer.writeheader()
+            for filename, tagMap in self.exportData.items():
+                row = self.generateTableRow(filename, tagMap, tags)
+                writer.writerow(row)
+
+    def generateTableRow(self, filename, tagMap, tags):
+        row = {"file": filename}
+        for tag in tags:
+            value = "-"
+            if tag in tagMap:
+                 value = tagMap[tag]["value"]
+            # Tag könnte hier gedreht werden zu besserer Lesbarkeit
+            row[tag] = value
+
+        return row
+
+
+
+exporter = TagExporter()
+exporter.saveTag("HalloTag", "Value1", "File00")
+exporter.saveTag("TschüssTag", "Value2", "File00")
+exporter.writeToFile("", ["HalloTag", "TschüssTag", "TestTag"])
