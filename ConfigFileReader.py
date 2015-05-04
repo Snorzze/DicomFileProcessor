945e5d401a30079e69fff31804fea59efad02a3f
thagel <thomashagel@gmx.net>
2015-05-04 13:43:36 +0200

    configfilereader gibt tags sowohl in richtiger als in falscher reihenfolge aus

--- ConfigFileReader.py
+++ ConfigFileReader.py
@@ -3,8 +3,7 @@ __author__ = 'Thomas and Kemal and Lukas'
 class ConfigFileReader:
 
     def readConfig(self, path):
-        text_file = open(path, "r")
-        content = text_file.read().strip().split(",")
+        self.content = self.readConfigCorrectOrder(path)
         return list(map(self.turn, content))
 
     def turn(self, tag):
@@ -16,3 +15,7 @@ class ConfigFileReader:
         part1 = tagHalf[:-2]
         part2 = tagHalf[-2:]
         return part2 + part1
+
+    def readConfigCorrectOrder(self,path):
+        text_file = open(path, "r")
+        return text_file.read().strip().split(",")
