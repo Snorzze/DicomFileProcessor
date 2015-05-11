102b6d4810f0696fb9d9ea871d88b91e30f8ef94
mkursche <mkursche@stud.hs-heilbronn.de>
2015-05-11 16:25:59 +0200

    clean code

--- test/ConfigFileReaderTest.py
+++ test/ConfigFileReaderTest.py
@@ -3,23 +3,26 @@ import os
 
 from ConfigFileReader import ConfigFileReader
 
+
 path = "./ConfigSamples/configSample1.txt"
 
-dir = os.path.dirname(__file__)
-path = os.path.join(dir, path)
+directory = os.path.dirname(__file__)
+path = os.path.join(directory, path)
 
 fileOrderTags = ['18005011', '18005111', '18005211']
 correctOrder = ['00181150', '00181151', '00181152']
 
+
 class TestConfigFileReader(unittest.TestCase):
     def setUp(self):
         self.configFileReader = ConfigFileReader()
-    def test_readConfig(self):
+
+    def test_read_config(self):
         result = self.configFileReader.readConfig(path)
         self.assertEquals(result, fileOrderTags)
         self.assertEquals(self.configFileReader.content, correctOrder)
 
-    def test_readConfigCorrectOrder(self):
+    def test_read_config_correct_order(self):
         result = self.configFileReader.readConfigCorrectOrder(path)
         self.assertEquals(result, correctOrder)
 
