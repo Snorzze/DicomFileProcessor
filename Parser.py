bf31470521d79ff75bf23a4ff048230fe0a5500d
mkursche <mkursche@stud.hs-heilbronn.de>
2015-05-11 16:57:45 +0200

    clean code

--- Parser.py
+++ Parser.py
@@ -17,22 +17,23 @@ class Parser:
 
     # NUR BIS PIXEL DATA SUCHEN ( 7FE0,0010 )
 
-    def parse_dicom_file(self, tagsearcher, pathtodicomfile):
+    def parse_dicom_file(self, tag_searcher, path_to_dicom_file):
         """
-        Parses the given dicom file for the tags, the tagsearcher includes
-        :param tagsearcher: includes the searched tags
-        :param pathtodicomfile: the path to the dicomdata
-        :return: a hasmap with all found tags as key and values
+        Parses the given dicom file for the tags, the tag_searcher includes
+
+        :param tag_searcher: includes the searched tags
+        :param path_to_dicom_file: the path to the dicom data
+        :return: a hash map with all found tags as key and values
         """
 
-        self.file = open(pathtodicomfile, "rb")
+        self.file = open(path_to_dicom_file, "rb")
         if self.find_dicom_start():
             export_map = {}
             tag = self.shift_byte_sequence(4)
             while tag is not None:
                 if tag == self.dicomImageStartTag:
                     return export_map
-                if tagsearcher.containsDicomTagInConfig(tag):
+                if tag_searcher.containsDicomTagInConfig(tag):
                     export_map[tag] = self.get_actual_tag_value()
                 else:
                     self.skip_actual_tag()
@@ -46,33 +47,9 @@ class Parser:
 
     def get_actual_tag_value(self):
         """
-        Reads automaticaly the value of the current loaded tag
+        Reads automatically the value of the current loaded tag
+
         :return: the value as string
-        """
-
-        self.shift_byte_sequence(4)
-        if self.is_valid_vr():
-            # Going for explicite Value --> little endian
-            if not self.is_special_vr():
-                # Normal vr --> normal process
-                lenght = struct.unpack("<H", (self.v3 + self.v4))[0]
-                return self.get_byte_sequence_as_ascii(lenght)
-            else:
-                # Specail vr --> special process
-                # skip reservated 2 bytes
-                self.shift_byte_sequence(4)
-                # read special lenght to read
-                lenght = struct.unpack("<I", (self.v1 + self.v2 + self.v3 + self.v4))[0]
-                return self.get_byte_sequence_as_ascii(lenght)
-        else:
-            # Going for implicit Value --> big endian
-            lenght = struct.unpack(">I", (self.v1 + self.v2 + self.v3 + self.v4))[0]
-            return self.get_byte_sequence_as_ascii(lenght)
-
-    def skip_actual_tag(self):
-        """
-        Skips actual loaded tag automaticly. Reads length and loads new bytes to the end of the value.
-        After execution of this method, the last four bytes from the tagvalue are loaded in the variables.
         """
 
         self.shift_byte_sequence(4)
@@ -80,34 +57,59 @@ class Parser:
             # Going for explicit Value --> little endian
             if not self.is_special_vr():
                 # Normal vr --> normal process
-                lenght_to_skip = struct.unpack("<H", (self.v3 + self.v4))[0]
-                self.shift_byte_sequence(lenght_to_skip)
+                length = struct.unpack("<H", (self.v3 + self.v4))[0]
+                return self.get_byte_sequence_as_ascii(length)
             else:
-                # Specail vr --> special process
-                # skip reservated 2 bytes and go to new length size
+                # Special vr --> special process
+                # skip reserved 2 bytes
                 self.shift_byte_sequence(4)
-                # read special lenght to skip
-                lenght_to_skip = struct.unpack("<I", (self.v1 + self.v2 + self.v3 + self.v4))[0]
-                self.shift_byte_sequence(lenght_to_skip)
+                # read special length to read
+                length = struct.unpack("<I", (self.v1 + self.v2 + self.v3 + self.v4))[0]
+                return self.get_byte_sequence_as_ascii(length)
         else:
             # Going for implicit Value --> big endian
-            lenght_to_skip = struct.unpack(">I", (self.v1 + self.v2 + self.v3 + self.v4))[0]
-            self.shift_byte_sequence(lenght_to_skip)
+            length = struct.unpack(">I", (self.v1 + self.v2 + self.v3 + self.v4))[0]
+            return self.get_byte_sequence_as_ascii(length)
 
-    def shift_byte_sequence(self, lengthofnewbytes):
+    def skip_actual_tag(self):
         """
-        Shifts the filereader for the given length.
+        Skips actual loaded tag automatically. Reads length and loads new bytes to the end of the value.
+        After execution of this method, the last four bytes from the tag value are loaded in the variables.
+        """
 
-        :param lengthofnewbytes: the length to shift
+        self.shift_byte_sequence(4)
+        if self.is_valid_vr():
+            # Going for explicit Value --> little endian
+            if not self.is_special_vr():
+                # Normal vr --> normal process
+                length_to_skip = struct.unpack("<H", (self.v3 + self.v4))[0]
+                self.shift_byte_sequence(length_to_skip)
+            else:
+                # Special vr --> special process
+                # skip reserved 2 bytes and go to new length size
+                self.shift_byte_sequence(4)
+                # read special length to skip
+                length_to_skip = struct.unpack("<I", (self.v1 + self.v2 + self.v3 + self.v4))[0]
+                self.shift_byte_sequence(length_to_skip)
+        else:
+            # Going for implicit Value --> big endian
+            length_to_skip = struct.unpack(">I", (self.v1 + self.v2 + self.v3 + self.v4))[0]
+            self.shift_byte_sequence(length_to_skip)
+
+    def shift_byte_sequence(self, length_of_new_bytes):
+        """
+        Shifts the file reader for the given length.
+
+        :param length_of_new_bytes: the length to shift
         :return: the last four bytes as string
         """
 
-        while lengthofnewbytes > 0:
+        while length_of_new_bytes > 0:
             self.v1 = self.v2
             self.v2 = self.v3
             self.v3 = self.v4
             self.v4 = self.file.read(1)
-            lengthofnewbytes -= 1
+            length_of_new_bytes -= 1
 
         if len(self.v4) != 0:
             return self.generate_actual_byte_sequence_as_string()
@@ -118,7 +120,7 @@ class Parser:
         """
         Reads the length of bytes and returns the ascii converted string
 
-        :param length: the lenght of the value to read
+        :param length: the length of the value to read
         :return: the converted string
         """
 
@@ -152,16 +154,16 @@ class Parser:
 
     def find_dicom_start(self):
         """
-        Reads the data till the dicomstarttag is found!
+        Reads the data till the dicom starttag is found!
         :return: If the tag was found
         """
 
-        bytesequence = self.shift_byte_sequence(4)
-        while bytesequence is not None:
-            if bytesequence == self.dicomStartBytePath:
+        byte_sequence = self.shift_byte_sequence(4)
+        while byte_sequence is not None:
+            if byte_sequence == self.dicomStartBytePath:
                 return True
             else:
-                bytesequence = self.shift_byte_sequence(1)
+                byte_sequence = self.shift_byte_sequence(1)
         return False
 
     def is_valid_vr(self):
@@ -172,30 +174,30 @@ class Parser:
 
         vr = self.convert_hex_to_ascii(self.v1) + self.convert_hex_to_ascii(
             self.v2)
-        validvrs = set(
+        valid_vrs = set(
             ["AE", "AS", "AT", "CS", "DA", "DS", "DT", "FL", "FD", "IS", "LO", "LT", "OB", "OF", "OW", "PN", "SH",
-             "SL",
-             "SQ", "SS", "ST", "TM", "UI", "UL", "UN", "US", "UT"])
-        return vr in validvrs
+             "SL", "SQ", "SS", "ST", "TM", "UI", "UL", "UN", "US", "UT"])
+        return vr in valid_vrs
 
     def is_special_vr(self):
         """
         Checks if vr is special vr, which has to read on a special way:
             - Skipping 2 bytes which are reserved for further dicom standards
-            - Reading 4 unsigned littlen endian Integers
+            - Reading 4 unsigned little endian Integers
+
         :return: if vr is a special vr
         """
 
         vr = self.convert_hex_to_ascii(self.v1) + self.convert_hex_to_ascii(
             self.v2)
-        specialvrs = set(["OB", "OW", "SQ", "UN"])
-        return vr in specialvrs
+        special_vrs = set(["OB", "OW", "SQ", "UN"])
+        return vr in special_vrs
 
     @staticmethod
     def convert_hex_to_string(hex_value):
         """
         :param hex_value: the byte to convert
-        :return: the bytes as string withoud 0x
+        :return: the bytes as string without 0x
         """
 
         return hex(ord(hex_value)).replace("0x", "")
@@ -205,7 +207,7 @@ class Parser:
         """
         Converts a hex value to a ascii encoded string
         :param hex_value: the value to encode
-        :return: the encode value or, if it wasn't a convertable hexbyte the byte as string
+        :return: the encode value or, if it wasn't a convertible hex-byte the byte as string
         """
 
         try:
